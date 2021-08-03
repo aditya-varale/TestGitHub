449d38a06b3f697b4bca8f9c9bf854ed19463572
AdityaVarale <87017809+AdityaVarale@users.noreply.github.com>
2021-08-03 16:52:51 +0530

    Update MainActivity.java
    Aditya Balance

--- app/src/main/java/com/example/mygitapplication/MainActivity.java
+++ app/src/main/java/com/example/mygitapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         findViewById();
         Listenrs();
+        int AdityaBalance = 10;
     }
 
     private void Listenrs() {
