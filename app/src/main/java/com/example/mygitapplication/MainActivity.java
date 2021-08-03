3dd721512a9d9803a24f9fe9c2f7ea13577bd8c7
aditya-varale <varalemcoe@gmail.com>
2021-08-03 16:19:22 +0530

    MyBranch_v1

--- app/src/main/java/com/example/mygitapplication/MainActivity.java
+++ app/src/main/java/com/example/mygitapplication/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById();
+        Listenrs();
+    }
+
+    private void Listenrs() {
     }
 
     private void findViewById() {
