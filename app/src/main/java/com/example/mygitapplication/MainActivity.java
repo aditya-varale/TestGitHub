64285238cee92808bbb916c74d13b078dd87ef24
AdityaVarale <87017809+AdityaVarale@users.noreply.github.com>
2021-08-03 16:30:54 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/mygitapplication/MainActivity.java
+++ app/src/main/java/com/example/mygitapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         findViewById();
         Listenrs();
+        int j = 10;
     }
 
     private void Listenrs() {
